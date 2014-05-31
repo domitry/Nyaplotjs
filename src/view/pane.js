791c312b639b66b0a8ad666bdf608c0d3937907f
domitry <domitry@gmail.com>
2014-05-31 10:52:13 +0900

    Refactor pane.js

--- src/view/pane.js
+++ src/view/pane.js
@@ -26,7 +26,7 @@ define([
 	var inner_width = options.width - options.margin.left - options.margin.right;
 	var inner_height = options.height - options.margin.top - options.margin.bottom;
 
-	var ranges = {x:[0,inner_width], y:[inner_height,0]}
+	var ranges = {x:[0,inner_width], y:[inner_height,0]};
 	var scales = {};
 
 	_.each({x:'xrange',y:'yrange'},function(val, key){
@@ -39,7 +39,13 @@ define([
 	model.append("g")
 	    .attr("transform", "translate(" + options.margin.left + "," + options.margin.top + ")");
 
-	var axis = new Axis(model.select("g"), scales, {width:inner_width, height:inner_height, grid:options.grid});
+	var axis = new Axis(model.select("g"), scales, {
+	    width:inner_width, 
+	    height:inner_height,
+	    grid:options.grid,
+	    x_label:options.x_label,
+	    y_label:options.y_label
+	});
 
 	model.select("g")
 	    .append("g")
@@ -52,28 +58,8 @@ define([
 	    .attr("width", inner_width)
 	    .attr("height", inner_height);
 
-	model.select("g")
-	    .append("text")
-	    .attr("x", inner_width/2)
-	    .attr("y", options.height- options.margin.bottom/1.5)
-	    .attr("text-anchor", "middle")
-	    .attr("fill", "black")
-	    .attr("font-size", 22)
-	    .text(options.x_label);
-
-	model.select("g")
-	    .append("text")
-	    .attr("x", -options.margin.left/1.5)
-	    .attr("y", inner_height/2)
-	    .attr("text-anchor", "middle")
-	    .attr("fill", "black")
-	    .attr("font-size", 22)
-	    .attr("transform", "rotate(-90," + -options.margin.left/1.5 + ',' + inner_height/2 + ")")
-	    .text(options.y_label);
-
-	this.model = model;
 	this.diagrams = [];
-	this.axis = axis;
+	this.context = model.select(".context");
 	this.scales = scales;
 	this.options = options;
 
@@ -81,17 +67,13 @@ define([
     }
 
     Pane.prototype.add = function(type, data, options){
-	var parent = this.model.select(".context");
-	var diagram = new diagrams[type](parent, this.scales, data, options);
+	var diagram = new diagrams[type](this.context, this.scales, data, options);
 	this.diagrams.push(diagram);
     };
 
     Pane.prototype.filter = function(target, options){
-	var parent = this.model.select(".context");
-	var axis = this.axis;
-	var scales = this.scales;
 	var diagrams = this.diagrams;
-	this.filter = new Filter(parent, scales, options);
+	this.filter = new Filter(this.context, this.scales, options);
 	this.filter.selected(function(ranges){
 	    _.each(diagrams, function(diagram){
 		diagram.checkIfSelected(ranges)
