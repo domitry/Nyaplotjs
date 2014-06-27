ddb7b1c31e7d2bbfe7cd0c67d36c35f835effeea
Pjotr Prins <pjotr.public01@thebird.nl>
2014-06-27 11:36:55 +0200

    Documented manager.js

--- src/core/manager.js
+++ src/core/manager.js
@@ -1,34 +1,45 @@
+/*
+ * Manager is the overall frame manager that holds plots and data
+ * sources (DataFrame).
+ */
+
 define([
     "underscore"
 ], function(_){
     var Manager = {data_frames: {}, panes: []};
 
+    // add a data source (DataFrame) by name
     Manager.addData = function(name, df){
 	entry = {};
 	entry[name] = df;
 	_.extend(this.data_frames, entry);
     };
 
+    // Fetch a data source by name
     Manager.getData = function(name){
 	return this.data_frames[name];
     };
 
+    // Add a pane to the manager
     Manager.addPane = function(pane){
 	this.panes.push(pane);
     };
 
+    // Tell the manager that data has been selected
     Manager.selected = function(data_id, rows){
 	_.each(this.panes, function(entry){
 	    entry.pane.selected(data_id, rows);
 	});
     };
 
+    // Update and redraw the panes
     Manager.update = function(){
 	_.each(this.panes, function(entry){
 	    entry.pane.update();
 	});
     };
 
+    // Update data (nyi)
     Manager.updateData = function(data_id, column_name, value){
 
     };
