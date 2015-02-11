96fe39d083314e0db254d45f80610342ee6e50f2
domitry <domitry@gmail.com>
2015-02-11 18:16:11 +0900

    Add some options to label

--- src/sheet/label.js
+++ src/sheet/label.js
@@ -1,18 +1,49 @@
 define([], function(){
     return [
         "label",
-        ["context"],
-        {},
-        function(context, options){
+        ["context", "width", "height", "x", "y"],
+        {
+            margin: {top:0,bottom:0,left:0,right:0},
+            rotate_x: 0,
+            rotate_y: 0
+        },
+        function(context, width, height, x_label, y_label, options){
             var g = context.append("g");
 
             g.append("text")
-                .attr("x", options.width/2)
-                .attr("y", options.height + options.margin.bottom/1.5)
+                .attr("x", width/2)
+                .attr("y", height + options.margin.bottom/1.5)
                 .attr("text-anchor", "middle")
                 .attr("fill", "rgb(50,50,50)")
                 .attr("font-size", 22)
-                .text(options.x_label);
+                .text(x_label);
+
+            g.append("text")
+                .attr("x", -options.margin.left/1.5)
+                .attr("y", height/2)
+                .attr("text-anchor", "middle")
+                .attr("fill", "rgb(50,50,50)")
+                .attr("font-size", 22)
+                .attr("transform", "rotate(-90," + -options.margin.left/1.5 + ',' + height/2 + ")")
+                .text(y_label);
+
+            if(options.rotate_y != 0){
+                g.selectAll(".y_axis")
+                    .selectAll("text")
+                    .style("text-anchor", "end")
+                    .attr("transform", function(d) {
+                        return "rotate(" + options.rotate_y + ")";
+                    });
+            }
+
+            if(options.rotate_x != 0){
+                g.selectAll(".x_axis")
+                    .selectAll("text")
+                    .style("text-anchor", "end")
+                    .attr("transform", function(d) {
+                        return "rotate(" + options.rotate_x + ")";
+                    });
+            }
 
             return g;
         }
