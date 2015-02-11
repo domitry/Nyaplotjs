2bcc27a383da2d5cf705ca37298cd6791ccc7e31
domitry <domitry@gmail.com>
2015-02-11 18:25:52 +0900

    Remove labeling function from axis

--- src/sheet/axis.js
+++ src/sheet/axis.js
@@ -17,22 +17,14 @@ define([
 ],function(_){
     return [
         "axis2d",
-        ["context", "xscale", "yscale"],
+        ["context", "xscale", "yscale", "width", "height"],
         {
-            width:0,
-            height:0,
             margin: {top:0,bottom:0,left:0,right:0},
             stroke_color:"#fff",
             stroke_width: 1.0,
-            x_label:'X',
-            y_label:'Y',
-            grid:true,
-            zoom:false,
-            zoom_range:[0.5, 5],
-            rotate_x_label:0,
-            rotate_y_label:0
+            grid:true
         },
-        function(context, xscale, yscale, options){
+        function(context, xscale, yscale, width, height, options){
             var xAxis = d3.svg.axis()
                     .scale(xscale)
                     .orient("bottom");
@@ -46,12 +38,16 @@ define([
             g.append("g").attr("class", "x_axis");
             g.append("g").attr("class", "y_axis");
 
+            if(options.grid){
+                xAxis.tickSize((-1)*height);
+                yAxis.tickSize((-1)*width);
+            }
+
             g.select(".x_axis").call(xAxis);
             g.select(".y_axis").call(yAxis);
 
             g.selectAll(".x_axis, .y_axis")
                 .selectAll("path, line")
-                .style("z-index", options.z_index)
                 .style("fill","none")
                 .style("stroke",options.stroke_color)
                 .style("stroke-width",options.stroke_width);
@@ -61,33 +57,10 @@ define([
                 .attr("fill", "rgb(50,50,50)");
 
             g.selectAll(".x_axis")
-                .attr("transform", "translate(0," + (options.height + 4) + ")");
+                .attr("transform", "translate(0," + (height + 4) + ")");
 
             g.selectAll(".y_axis")
                 .attr("transform", "translate(-4,0)");
-            
-            if(options.rotate_y_label != 0){
-                g.selectAll(".y_axis")
-                    .selectAll("text")
-                    .style("text-anchor", "end")
-                    .attr("transform", function(d) {
-                        return "rotate(" + options.rotate_y_label + ")";
-                    });
-            }
-
-            if(options.rotate_x_label != 0){
-                g.selectAll(".x_axis")
-                    .selectAll("text")
-                    .style("text-anchor", "end")
-                    .attr("transform", function(d) {
-                        return "rotate(" + options.rotate_x_label + ")";
-                    });
-            }
-
-            if(options.grid){
-                xAxis.tickSize((-1)*options.height);
-                yAxis.tickSize((-1)*options.width);
-            }
 
             return g;
         }
