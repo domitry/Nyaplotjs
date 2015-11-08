c3e98b8c6fd4562c03951ea64026072199083a4c
Naoki Nishida <domitry@gmail.com>
2015-11-09 03:50:15 +0900

    Add State#update to avoid NotImplemented error
    State#update should be over-written by parser.

--- src/state.js
+++ src/state.js
@@ -2,6 +2,9 @@ define([
     'underscore'
 ],function(_){
     function State(obj){
+        this.update = function(){
+            console.warn("State#update was called but it has no side-effect.");
+        };
         if(_.isUndefined(obj))obj={};
         _.extend(this, obj);
     }
