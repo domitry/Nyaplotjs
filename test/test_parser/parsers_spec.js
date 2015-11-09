c778bbf5e53f11cdc88388b2d183ba3fdb715119
Naoki Nishida <domitry@gmail.com>
2015-11-10 01:02:15 +0900

    Add spec common to all parsers

---
+++ test/test_parser/parsers_spec.js
@@ -0,0 +1,33 @@
+/*global define, describe, it, expect, require*/
+
+define([
+    "underscore",
+    "parser/stage2d",
+    "parser/scale",
+    "parser/position",
+    "parser/pane",
+    "parser/data",
+    "parser/row_scale",
+    "parser/df_scale"
+], function(_, stage, scale, position, pane, data, row_scale, df_scale){
+    /*
+     * All parser module should return the array of:
+     *  name of parser (string)
+     *  required arguments (array)
+     *  optional arguments (object)
+     *  parser (function)
+     */
+    (function(){
+        var modules = _.select([].slice.call(arguments, 1));
+        _.each(modules, function(module){
+            describe(module[0] + "parser test", function(){
+                it("should return name, array, object, function", function(){
+                    expect(_.isString(module[0])).to.be(true);
+                    expect(_.isArray(module[1])).to.be(true);
+                    expect(_.isObject(module[2])).to.be(true);
+                    expect(_.isFunction(module[3])).to.be(true);
+                });
+            });
+        });
+    })();
+});
