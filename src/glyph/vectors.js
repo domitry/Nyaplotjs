0578938c66053bdfe5234ad4c11b71882da0af46
Naoki Nishida <domitry@gmail.com>
2015-11-18 03:09:18 +0900

    Add the option 'with_arrow' to glyph/vectors

--- src/glyph/vectors.js
+++ src/glyph/vectors.js
@@ -7,11 +7,31 @@ define([
         {
             color:'steelblue',
             stroke_color: '#000',
-            stroke_width: 2
+            stroke_width: 2,
+            with_arrow: false
         },
         function(context, data, x1, y1, x2, y2, position, options){
             var shapes = context.selectAll("line").data(data.asarray());
             var p1 = position(x1, y1), p2 = position(x2, y2);
+
+            if(options.with_arrow){
+                context.append("defs").append("marker")
+                    .attr({
+                        id: "arrow",
+                        viewBox: '0 0 10 10',
+                        refX: 8,
+                        refY: 5,
+                        markerWidth: 8,
+                        markerHeight: 8,
+                        orient: "auto"
+                    })
+                    .append('path')
+                    .attr({
+                        d: 'M10 5 0 0 0 10Z',
+                        stroke: 'none',
+                        fill: 'black'
+                    });
+            }
             
             shapes.enter()
                 .append("line")
@@ -22,7 +42,8 @@ define([
                     'y2': p2.y,
                     'fill': options.color,
                     'stroke': options.stroke_color,
-                    'stroke-width':options.stroke_width
+                    'stroke-width':options.stroke_width,
+                    'marker-end': (options.with_arrow ? 'arrow' : 'none')
                 });
 
             return shapes;
