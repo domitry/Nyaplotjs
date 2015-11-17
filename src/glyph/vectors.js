83b053ba54d722230b8752b810423f49dbb8827b
Naoki Nishida <domitry@gmail.com>
2015-11-18 02:58:11 +0900

    Modernize glyph/vectors

--- src/glyph/vectors.js
+++ src/glyph/vectors.js
@@ -23,23 +23,23 @@ define([
 ],function(_){
     return [
         "vectors",
-        ["context", "data", "x", "y", "dx", "dy", "position"],
+        ["context", "data", "x1", "y1", "x2", "y2", "position"],
         {
             color:'steelblue',
             stroke_color: '#000',
-            stroke_width: 2,
-            hover: true
+            stroke_width: 2
         },
-        function(context, data, x, y, dx, dy, position, options){
-            var shapes = context.selectAll("line").data(data);
-
+        function(context, data, x1, y1, x2, y2, position, options){
+            var shapes = context.selectAll("line").data(data.asarray());
+            var p1 = position(x1, y1), p2 = position(x2, y2);
+            
             shapes.enter()
                 .append("line")
                 .attr({
-                    'x1':function(d){return position(d[x], d[y]).x;},
-                    'x2':function(d){return position(d[x] + d[dx], d[y] + d[dy]).x;},
-                    'y1':function(d){return position(d[x], d[y]).y;},
-                    'y2':function(d){return position(d[x] + d[dx], d[y] + d[dy]).y;},
+                    'x1': p1.x,
+                    'x2': p1.y,
+                    'y1': p2.x,
+                    'y2': p2.y,
                     'fill': options.color,
                     'stroke': options.stroke_color,
                     'stroke-width':options.stroke_width
