3fee892270e0ff769d414a25f0972b8d6ba3b757
Naoki Nishida <domitry@gmail.com>
2015-11-18 02:59:42 +0900

    Add glyph/text

---
+++ src/glyph/text.js
@@ -0,0 +1,29 @@
+define([
+    'underscore'
+], function(_){
+    return [
+        "text",
+        ["context", "data", "text", "x", "y", "position"],
+        {
+            color: "black",
+            font_size: 10,
+            text_anchor: "middle"
+        },
+        function(context, data, text, x, y, position, options){
+            var pos = position(x, y);
+            return context
+                .selectAll("text")
+                .data(data.asarray())
+                .enter()
+                .append("text")
+                .attr({
+                    cx: pos.x,
+                    cy: pos.y,
+                    fill: options.color,
+                    "font-size": options.font_size,
+                    "text-anchor": options.text_anchor
+                })
+                .text(text);
+        }
+    ];
+});
