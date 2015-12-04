f38c70475af6cbfcc02591aa0f079303bca31096
Naoki Nishida <domitry@gmail.com>
2015-12-04 13:34:06 +0900

    No longer animate when creating scatter
    It prevents proper rendering (related to calculation of width and height)

--- src/glyph/scatter.js
+++ src/glyph/scatter.js
@@ -22,7 +22,6 @@ define([
                 .attr("fill", options.color)
                 .attr("stroke", options.stroke_color)
                 .attr("stroke-width", options.stroke_width)
-                .transition().duration(200)
                 .attr("d", d3.svg.symbol().type(options.shape).size(options.size));
             
             context.selectAll("path")
