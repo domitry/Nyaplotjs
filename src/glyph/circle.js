3477245606b7347af91159ae55e14286378a1133
Naoki Nishida <domitry@gmail.com>
2015-12-04 13:30:07 +0900

    Allow to specify arbitary value as radius of circle

--- src/glyph/circle.js
+++ src/glyph/circle.js
@@ -7,23 +7,46 @@ define([
         {
             radius: 100,
             color: "steelblue",
-            stroke_width: 1,
-            stroke_color: "black"
+            stroke_color: "black",
+            stroke_width: 0.0,
+            opacity: 1.0
         },
-        function(context, data, cx, cy, position, options){
+        function(g, data, cx, cy, position, options){
             var pos = position(cx, cy);
-            return context
+            
+            g
                 .selectAll("circle")
                 .data(data.asarray())
                 .enter()
                 .append("circle")
                 .attr({
+                    r: 1.0,
+                    fill: options.color,
+                    "stroke-width": options.stroke_width,
+                    "fill-opacity": options.opacity,
+                    "stroke-opacity": options.opacity,
+                    stroke: options.stroke_color
+                });
+
+            g.selectAll("circle")
+                .attr({
                     cx: pos.x,
                     cy: pos.y,
-                    r: options.radius,
-                    fill: options.color,
-                    stroke_width: options.stroke_width,
-                    stroke: options.stroke_color
+                    transform: function(d){
+                        var next = {};
+                        next[cx] = d[cx] + options.radius;
+                        next[cy] = 0;
+                        var sx = Math.abs(pos.x(d) - pos.x(next));
+
+                        next[cx] = 0;
+                        next[cy] = d[cy] + options.radius;
+                        var sy = Math.abs(pos.y(d) - pos.y(next));
+
+                        var cx_ = pos.x(d);
+                        var cy_ = pos.y(d);
+
+                        return "matrix(" + sx + ",0,0," + sy + "," + (cx_-sx*cx_).toString() + "," + (cy_-sy*cy_).toString() + ")";
+                    }
                 });
         }
     ];
