736b37355bf66e8f582fbae3d4ad55281f6d47f0
Naoki Nishida <domitry@gmail.com>
2015-12-04 13:12:03 +0900

    Remove needless dependencies

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 require.config({
     paths: {
-        'underscore': '../contrib/underscore/underscore',
-        'node-uuid':'../contrib/node-uuid/uuid',
-        'colorbrewer': '../contrib/colorbrewer/colorbrewer'
+        'underscore': '../contrib/underscore/underscore'
     }
 });
