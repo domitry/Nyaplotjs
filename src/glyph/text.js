7cb059b3b340268b8e1721a994bfaafb85a5cf55
Naoki Nishida <domitry@gmail.com>
2015-12-04 13:35:21 +0900

    Add the option 'dominant-baseline' to text

--- src/glyph/text.js
+++ src/glyph/text.js
@@ -5,25 +5,33 @@ define([
         "text",
         ["data", "x", "y", "position"],
         {
+            text: "",
             color: "black",
-            font_size: 10,
-            text_anchor: "middle"
+            font_size: 16,
+            text_anchor: "start",
+            dominant_baseline: "middle"
         },
-        function(context, data, text, x, y, position, options){
+        function(g, data, x, y, position, options){
             var pos = position(x, y);
-            return context
+            g
                 .selectAll("text")
                 .data(data.asarray())
                 .enter()
                 .append("text")
                 .attr({
-                    cx: pos.x,
-                    cy: pos.y,
                     fill: options.color,
                     "font-size": options.font_size,
-                    "text-anchor": options.text_anchor
+                    "text-anchor": options.text_anchor,
+                    "dominant-baseline": options.dominant_baseline
                 })
-                .text(text);
+                .text(options.text);
+
+            return g
+                .selectAll("text")
+                .attr({
+                    x: pos.x,
+                    y: pos.y
+                });
         }
     ];
 });
