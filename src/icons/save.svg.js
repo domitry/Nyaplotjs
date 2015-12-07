8a36aaf9c772b307ef0b1cce467313a3e6e59c33
Naoki Nishida <domitry@gmail.com>
2015-12-07 14:11:42 +0900

    Change the format of svg from base64 to raw xml

---
+++ src/icons/save.svg.js
@@ -0,0 +1,12 @@
+define([], function(){
+    return "<!--?xml version=\"1.0\" encoding=\"utf-8\"?-->\
+\
+<svg version=\"1.1\" id=\"_x32_\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\" viewBox=\"0 0 512 512\" style=\"width: 20px; height: 20px; opacity: 1;\" xml:space=\"preserve\">\
+<g>\
+	<path class=\"st0\" d=\"M502.394,106.098L396.296,0h-15.162v121.49H130.866V0H60.27C26.987,0,0,26.987,0,60.271v391.458\
+		C0,485.013,26.987,512,60.27,512h391.459C485.014,512,512,485.013,512,451.729V129.286\
+		C512,120.591,508.542,112.256,502.394,106.098z M408.39,428.121H103.609V216.944H408.39V428.121z\" style=\"fill: rgb(222, 222, 222);\"></path>\
+	<rect x=\"282.012\" class=\"st0\" width=\"68.027\" height=\"94.015\" style=\"fill: rgb(222, 222, 222);\"></rect>\
+</g>\
+</svg>";
+});
